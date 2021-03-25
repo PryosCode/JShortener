2b8ad3ca74d4cccc71082fbd9e6af6fdc9c56400
PryosCode <info@pryoscode.net>
2021-03-25 17:06:12 +0100

    Update Dockerfile + gradle.yml

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
-FROM openjdk:11-jre-slim
-WORKDIR /jshortener
+FROM openjdk:11-jdk-slim-buster
 EXPOSE 80
 
 RUN apt-get update
 RUN apt-get -y install git
 
+WORKDIR /gradle
 RUN git clone https://github.com/PryosCode/JShortener.git .
 RUN chmod +x gradlew
 RUN ./gradlew shadowJar
 
-ENTRYPOINT java -jar JShortener.jar
+WORKDIR /jshortener
+ENTRYPOINT java -jar /gradle/build/libs/JShortener.jar
--- src/main/java/net/pryoscode/jshortener/log/LogHandler.java
+++ src/main/java/net/pryoscode/jshortener/log/LogHandler.java
@@ -1,14 +1,19 @@
 package net.pryoscode.jshortener.log;
 
 import java.util.logging.Handler;
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 public class LogHandler extends Handler {
 
     @Override
     public void publish(LogRecord record) {
-        String out = "[" + record.getLevel().getName().toUpperCase() + "] " + record.getMessage();
-        System.out.println(out);
+        String color = "";
+        if (Level.WARNING.equals(record.getLevel()))
+            color = LogColor.YELLOW;
+        else if (Level.SEVERE.equals(record.getLevel()))
+            color = LogColor.RED;
+        System.out.println(color + "[" + record.getLevel().getName().toUpperCase() + "] " + record.getMessage() + LogColor.RESET);
     }
 
     @Override
